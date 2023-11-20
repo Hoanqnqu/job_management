be2b6357935e6320f2e1ed1866c9de37aaec92a2
Hoanqnqu <nhathoangthuychaub1@gmail.com>
2023-11-20 18:34:07 +0700

    feature: add styled in landing _ completed part 17

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 
+import React from 'react'
 import './App.css'
+import Landing from './pages/Landing'
 
 function App() {
-  
-
   return (
-    <>
-      <div>
-       <p>Hello</p>
-      </div>
-    </>
+    <div>
+      <Landing/>
+    </div>
   )
 }
 
