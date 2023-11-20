aa6ee42c7676411da2663d536294db9a26ae4125
Hoanqnqu <nhathoangthuychaub1@gmail.com>
2023-11-20 19:18:40 +0700

    feature: add error Page_completed part 18

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 
 import React from 'react'
-import './App.css'
-import Landing from './pages/Landing'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Error, Landing, Register, Dashboard } from './pages';
 
 function App() {
   return (
-    <div>
-      <Landing/>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Dashboard />} />
+      <Route path='landing' element={<Landing />} />
+      <Route path='register' element={<Register />} />
+      <Route path='*' element={<Error />} />
+    </Routes>
+  </BrowserRouter>
   )
 }
 
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,13 +1,15 @@
-import logo from '../assets/images/logo.svg';
 import main from '../assets/images/main.svg';
 import React from 'react'
-import styled from 'styled-components';
+import Wrapper from '../assets/wrappers/LandingPage'
+import { Logo } from '../components';
+import { Link } from 'react-router-dom';
 
 const Landing = () => {
+
     return (
       <Wrapper>
         <nav>
-          <img src={logo} alt='jobster logo' className='logo' />
+        <Logo/>        
         </nav>
         <div className='container page'>
           {/* info */}
@@ -16,7 +18,7 @@ const Landing = () => {
               job <span>tracking</span> app
             </h1>
             <p>This absolutely blew me away! The way the music and the imagery worked together was truly magical. It was clear that a lot of thought and creativity went into the making of this song, and it really paid off. I was completely transported to another world as I watched, and I couldn't stop thinking about it for days afterward. Kudos to the artist and creators who put this masterpiece together!</p>
-            <button className='btn btn-hero'>Login/Register</button>
+            <Link to='/register' className='btn btn-hero'>Login/Register</Link>
           </div>
           <img src={main} alt='job hunt' className='img main-img' />
         </div>
@@ -24,42 +26,5 @@ const Landing = () => {
     );
   };
 
-const Wrapper = styled.main`
-nav{
-  width: var(--fluid-width);
-  max-width: var(--max-width);
-  margin: 0 auto;
-  height: var(--nav-height);
-  display: flex;
-  align-items: center;
-}
-.page {
-  min-height: calc(100vh - var(--nav-height));
-  display: grid;
-  align-items: center;
-  margin-top: -3rem;
-}
-h1 {
-  font-weight: 700;
-  span {
-    color: var(--primary-500);
-  }
-}
-p {
-  color: var(--grey-600);
-}
-.main-img{
-  display: none;
-}
-@media (min-width: 992px) {
-  .page {
-    grid-template-columns: 1fr 1fr;
-    column-gap: 3rem;
-  }
-  .main-img{
-    display: block;
-  }
-}
-`
 
   export default Landing;
