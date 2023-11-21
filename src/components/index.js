c44fa0c9c9dd6e9a3f839aa75f98a6244ad4251d
Hoanqnqu <nhathoangthuychaub1@gmail.com>
2023-11-21 16:49:47 +0700

    Completed part 24: AllJobs

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,8 @@ import Logo from "./Logo";
 import FormRow from "./FormRow";
 import BigSidebar from "./BigSidebar";
 import Navbar from "./Navbar";
-import SmallSidebar from "./SmallSideBar";
-export { Logo, FormRow, BigSidebar, Navbar, SmallSidebar }
+import SmallSidebar from "./SmallSidebar";
+import SearchContainer from "./SearchContainer";
+import JobsContainer from "./JobsContainer";
+import Loading from "./Loading";
+export { Logo, FormRow, BigSidebar, Navbar, SmallSidebar, JobsContainer, SearchContainer, Loading };
